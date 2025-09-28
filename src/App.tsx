fc0d686c29fc0372ec4cc64adb74619e9a2693e9
maci-kb24 <macikb8@yahoo.com>
2025-09-28 19:05:35 +0200

    add custom hook useDebounce

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { ItemList } from "./components/ItemCard/ItemList/ItemList"
 
 
 function App() {
 
   return (
-    <>
-     
-    </>
+   <div className="min-h-screen bg-gray-50">
+    <header></header>
+    <main className="max-w-6xl mx-auto px-4 py-8 space-y-8">
+      <section aria-label="Product list">
+          <ItemList items={[]} onDeleteItem={() => {}} />
+        </section>
+    </main>
+    <footer></footer>
+   </div>
   )
 }
 
