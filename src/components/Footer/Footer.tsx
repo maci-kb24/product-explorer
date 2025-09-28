b465d7a2d1331157a34e255257008e5a328efed8
maci-kb24 <macikb8@yahoo.com>
2025-09-28 19:07:14 +0200

    created the footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => (
+  <footer className="bg-white border-t border-gray-200 mt-12">
+    <div className="max-w-6xl mx-auto px-4 py-6 text-center text-gray-600">
+      <p>React Product Explorer - Built with React, TypeScript & Tailwind CSS</p>
+    </div>
+  </footer>
+);
+
+export { Footer };
